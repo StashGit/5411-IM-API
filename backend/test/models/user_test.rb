91d4a4a4574e97299bf19496df9e7aea2caee477
amiralles <amiralles.net@gmail.com>
2020-03-07 12:25:54 -0300

    add user tests.

--- backend/test/models/user_test.rb
+++ backend/test/models/user_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "an user can be admin" do
+    user = User.new email: "admin@example.com"
+    user.is_admin = true
+    user.save
+
+    assert user.is_admin?
+  end
+
+  test "the user email must be unique" do
+    assert_raise ActiveRecord::RecordInvalid do
+      User.create! email: "joe@example.com"
+      User.create! email: "joe@example.com"
+    end
+  end
+
+  test "the user email is required" do
+    assert_raise ActiveRecord::RecordInvalid do
+      User.create! email: nil
+    end
+  end
+
+  test "by default an user is not an admin" do
+    assert_raise ActiveRecord::RecordInvalid do
+      user = User.create! email: "joe@example.com"
+      assert !user.is_admin?
+    end
+  end
 end
