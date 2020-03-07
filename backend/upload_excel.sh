08af61ec14480de14e47b2340f0966012ded0002
amiralles <amiralles.net@gmail.com>
2020-03-07 19:23:17 -0300

    update testing script.

--- backend/upload_excel.sh
+++ backend/upload_excel.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Gets or creates an access tocken.
+# Uploads an excel file an creates the stock transactions.
+# It's important to use a *valid* access tocken. If this token
+# doesn't works, create a brand new one by running the create_session script.
 curl -H "Content-Type: multipart/mixed"   \
 	 -H "Accepts: application/json" \
 	 -H "Access-Token: 1a095eeeb9783e43b8369171e1dbf98a" \
