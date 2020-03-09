6d38e3d2af398c462450008ac09e46f22e4d8c36
amiralles <amiralles.net@gmail.com>
2020-03-09 10:39:06 -0300

    Muevo stock tests a su propio archivo.

--- backend/test/models/packing_list_parser_test.rb
+++ backend/test/models/packing_list_parser_test.rb
@@ -52,19 +52,6 @@ class PackingListParserTest < ActiveSupport::TestCase
     assert_equal skus * sizes, entries.count
   end
 
-  test "generate stock transactions from packing list" do
-    parser = PackingListParser.new(pl_path)
-    entries = parser.parse
-    Stock.create(entries, User.new(email: "john@example.com"))
-
-    # Verifico un par de transacciones.
-    sku = Sku.new(style: "SS200105S", color: "MIDNIGHT", size: "AU6 US2")
-    assert_equal(4, Stock.units(sku))
-
-    sku = Sku.new(style: "SS200106D", color: "MIDNIGHT", size: "AU10 US6")
-    assert_equal(15, Stock.units(sku))
-  end
-
   private
   
   def pl_path
---
+++ backend/test/models/stock_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class StockTransactionTest < ActiveSupport::TestCase
+  test "generate stock transactions from packing list" do
+    parser = PackingListParser.new(pl_path)
+    entries = parser.parse
+    Stock.create(entries, User.new(email: "john@example.com"))
+
+    # Verifico un par de transacciones.
+    sku = Sku.new(style: "SS200105S", color: "MIDNIGHT", size: "AU6 US2")
+    assert_equal(4, Stock.units(sku))
+
+    sku = Sku.new(style: "SS200106D", color: "MIDNIGHT", size: "AU10 US6")
+    assert_equal(15, Stock.units(sku))
+  end
+  
+  private
+
+  def pl_path
+    file_fixture('pl1.xlsx').to_s
+  end
+end
