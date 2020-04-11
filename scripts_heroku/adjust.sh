9b64e2bf843d4a5d90efa2bad2e10a0fa1196ad8
amiralles <amiralles.net@gmail.com>
2020-04-10 21:41:52 -0300

    Add script adjustments heroku.

---
+++ scripts_heroku/adjust.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TOKEN=$1
+BRAND_ID=$2
+UNITS=$3
+REASON=$4
+
+if [[ $TOKEN && $BRAND_ID ]];
+then
+    curl -H "Content-Type: application/json" \
+         -H "Access-Token: $TOKEN" \
+         -X POST \
+         -d "{ \"brand_id\": \"$BRAND_ID\", \"style\": \"SS200105S\", \"color\": \"MIDNIGHT\", \"size\": \"AU6 US2\" ,\"units\":"$UNITS", \"comments\":\"This is a comment.\", \"reason\" : \"$REASON\" }" \
+	     stock-api-5411.herokuapp.com/stock/adjust
+else
+    echo "Must provide a valid access token and a brand id."
+fi
+
