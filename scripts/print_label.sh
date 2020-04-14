aa40f047bcb839efad7e74a1f95bb9f38ba358db
amiralles <amiralles.net@gmail.com>
2020-04-14 16:53:01 -0300

    udpate print_labels script.

--- scripts/print_label.sh
+++ scripts/print_label.sh
@@ -2,15 +2,16 @@
 
 TOKEN=$1
 ID=$2
+COPIES=$3
 
-if [[ $TOKEN && $ID ]];
+if [[ $TOKEN && $ID && $COPIES ]];
 then
     curl -H "Content-Type: application/json" \
          -H "Access-Token: $TOKEN" \
          -X POST \
-         -d "{ \"id\": $ID }" \
+         -d "{ \"id\": $ID, \"copies\": $COPIES }" \
          localhost:3000/stock/print_label
 else
-    echo "Must provide a valid access token and a QR ID."
+    echo "Must provide a valid access token, QR ID and the number of copies."
 fi
 
