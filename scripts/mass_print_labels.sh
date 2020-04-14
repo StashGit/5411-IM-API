bf079079d3407d696a9fbceac874eb3c86d5af35
amiralles <amiralles.net@gmail.com>
2020-04-14 16:58:15 -0300

    update mass_print_labels script.

--- scripts/mass_print_labels.sh
+++ scripts/mass_print_labels.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 TOKEN=$1
+ID=$2
+COPIES=$3
 
-if [[ $TOKEN ]];
+if [[ $TOKEN && $ID && $COPIES ]];
 then
     curl -H "Content-Type: application/json" \
          -H "Access-Token: $TOKEN" \
          -X POST \
-         -d "{ \"qr_ids\": [36, 37, 38, 39, 40] }" \
+         -d "{ \"qrs\": [ { \"id\": $ID, \"copies\": $COPIES } ] }" \
          localhost:3000/stock/mass_print_labels
 else
-    echo "Must provide a valid access token."
+    echo "Must provide a valid access token, QR ID and the number of copies."
 fi
 
