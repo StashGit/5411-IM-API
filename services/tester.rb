855dbc3efc2c330c2a0e1a38e5e6447c85b24747
amiralles <amiralles.net@gmail.com>
2020-06-06 15:02:41 -0300

    change default lookup path.

--- services/tester.rb
+++ services/tester.rb
@@ -12,7 +12,7 @@ class Tester
   include ::Label
 
   def initialize()
-    root_path = ENV['PRINT_ROOT'] || "/Users/amiralles/dev/stash/5411-IM-API/services/tests"
+    root_path = ENV['PRINT_ROOT'] || "." #"/Users/amiralles/dev/stash/5411-IM-API/services/tests"
     @qrs_path  = File.join(root_path, "qr")
     set_qr_root root_path
     set_lbl_root root_path
