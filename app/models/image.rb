7cf600fb9217fc404e8ea6dd5d03cec8393df911
amiralles <amiralles.net@gmail.com>
2020-12-29 23:46:47 -0300

    Add describe to images.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,11 @@
 class Image < ApplicationRecord
+  def describe
+    image_description.new(self.id, self.s3_key, self.url)
+  end
+
+  private
+
+  def image_description
+    @image_description ||= Struct.new(:id, :s3_key, :url)
+  end
 end
